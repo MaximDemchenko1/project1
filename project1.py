de987950668706217f2bf69a331cf90835f8a34a
MaximDemchenko1 <max1mdemch@icloud.com>
2024-09-05 20:36:35 +0300

    create file project6

--- project1.py
+++ project1.py
@@ -1,1 +1,26 @@
-"fkjsjn"
+per1= 1
+per2= 100
+print(per1**per2)
+per3= 2
+per4= 16
+print(per3**per4)
+per5=2
+per6= 10000
+print(per5-per6)
+print(per3+per3*per3)
+per7= 60
+per8= 3
+print(per7/per8)
+import math
+per9= 144
+print(math.sqrt(per9))
+per10= 5
+per11= 14
+print(per3+per10*per11)
+per12= 66
+per13= 14
+per14= None
+print(type(str(per14)))
+str1= "bull"
+print(str1[1])
+
