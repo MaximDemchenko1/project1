87caaa5ae8a525b11d68de23d9309a999771e921
MaximDemchenko1 <max1mdemch@icloud.com>
2024-09-06 17:23:27 +0300

    create file project5

--- project6.py
+++ project6.py
@@ -1,18 +1,6 @@
-year1=int(input("Введите год рождения первого человека"))
-mon1=int(input("Введите месяц рождения первого человека"))
-day1=int(input("Введите день рождения первого человека"))
-year2=int(input("Введите год рождения второго человека"))
-mon2=int(input("Введите месяц рождения второго человека"))
-day2=int(input("Введите день рождения второго человека"))
-import datetime
-str1=datetime.date(year1,mon1,day1)
-str2=datetime.date (year2,mon2,day2)
-str3=datetime.date.today()
-str4=str3-str1
-str5=str3-str2
-if str4>str5:
-    print ("Первый человек старше")
-elif str4==str5:
-    print ("О динаковый возраст")
-else:
-    print ("Второй человек старше")
+str1=int(input("Введите минимальное значение:"))
+str2=int(input("Введите максимальное значение:"))
+str3=int(input("Введите шаг:"))
+for i in range (str1,str2+1,str3):
+    print (i)
+
